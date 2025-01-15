75ea08fdbcae5b9cfaf029e882e88969e3768e39
Reina_Sakiria <rs@rs64.net>
2025-01-15 16:44:45 +0900

    fix: syntax error

--- .github/workflows/JsScripts/GetPackageDotJson.js
+++ .github/workflows/JsScripts/GetPackageDotJson.js
@@ -1,8 +1,12 @@
 const { Assets } = process.env
 
-for (const asset of JSON.parse(Assets)) {
-    if (asset.name === "package.json") {
-        const res = await fetch(asset.browser_download_url);
-        return await res.text();
-    }
-}
+(async () => {
+
+        for (const asset of JSON.parse(Assets)) {
+            if (asset.name === "package.json") {
+                const res = await fetch(asset.browser_download_url);
+                return await res.text();
+            }
+        }
+
+    })();
